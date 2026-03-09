ed6ca728656c9a46885c454c600b02e7ae5400c8
quangnek1 <leduyquang1201@gmail.com>
2026-03-09 18:06:11 +0700

    DBSeeding

--- src/core/Spa.WebAPI/Program.cs
+++ src/core/Spa.WebAPI/Program.cs
@@ -1,5 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Spa.Domain.Entities.Identity;
 using Spa.Infrastructure.Common;
 using Spa.Infrastructure.Data;
 
@@ -13,12 +15,44 @@ try
 	builder.Services.AddDbContext<ApplicationDbContext>(options =>
 		options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+	// 2. THÊM ĐOẠN NÀY VÀO: Đăng ký Identity
+	builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+	{
+		// Bạn có thể cấu hình rule mật khẩu ở đây nếu muốn
+		options.Password.RequireDigit = true;
+		options.Password.RequireLowercase = true;
+		options.Password.RequireNonAlphanumeric = true;
+		options.Password.RequireUppercase = true;
+		options.Password.RequiredLength = 6;
+	})
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	.AddDefaultTokenProviders();
+
+	// 3. Đăng ký class Seeder vào DI Container
+	builder.Services.AddScoped<SpaDbInitializer>();
+
 	builder.Services.AddControllers();
 	// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 	builder.Services.AddOpenApi();
 
 	var app = builder.Build();
- 
+
+	// 2. Chạy Seeder lúc khởi động ứng dụng
+	using (var scope = app.Services.CreateScope())
+	{
+		var services = scope.ServiceProvider;
+		try
+		{
+			var seeder = services.GetRequiredService<SpaDbInitializer>();
+			await seeder.SeedAsync();
+		}
+		catch (Exception ex)
+		{
+			var logger = services.GetRequiredService<ILogger<Program>>();
+			logger.LogError(ex, "Đã xảy ra lỗi trong quá trình khởi tạo dữ liệu mẫu.");
+		}
+	}
+
 
 	// Configure the HTTP request pipeline.
 	if (app.Environment.IsDevelopment())
